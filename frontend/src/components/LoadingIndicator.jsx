f32c631425da99dc97bf6c6e81f4ab2d5473485f
Tomislav <tomislav@MBP-od-Ivan.home>
2024-11-12 13:11:21 +0100

    first commit

---
+++ frontend/src/components/LoadingIndicator.jsx
@@ -0,0 +1,9 @@
+import '../styles/LoadingIndicator.css'
+
+const LoadingIndicator = () => {
+    return <div>
+        <div className='loader'></div>
+    </div>
+}
+
+export default LoadingIndicator
